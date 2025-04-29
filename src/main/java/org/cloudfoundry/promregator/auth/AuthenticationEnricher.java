c8508b6a17e352f4a2d456e453b1b6073c0ac483
vladaunski <v.barbulescu@student.tue.nl>
2025-04-29 21:10:53 +0200

    test

--- src/main/java/org/cloudfoundry/promregator/auth/AuthenticationEnricher.java
+++ src/main/java/org/cloudfoundry/promregator/auth/AuthenticationEnricher.java
@@ -1,5 +1,4 @@
 package org.cloudfoundry.promregator.auth;
-import org.cloudfoundry.promregator.auth.impl.AuthenticationEnricherImpl;
 import org.apache.http.client.methods.HttpGet;
 
 public interface AuthenticationEnricher {
